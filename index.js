fe39c3f1e2dee295e6d5d340865ceb7e8d5cd280
maxitabakman <49928297@est.ort.edu.ar>
2025-09-04 11:20:59 -0300

    termine las dos manijas

--- index.js
+++ index.js
@@ -3,7 +3,12 @@ import {generarEspacio} from "./busqueda.js";
  let array =generarEspacio(100)
  
 for(let i =0;i <array.length;i++){
- 
-if (array [i]=== "Wally")
-    console.log (i+") " + array[i])
-}   console.log(array)
+if (array [i]=== "Wally"){
+    console.log (i+") " + array[i])}}
+    for(let i =0;i <array.length;i++){ 
+if (array [i].indexOf("w")=== 0){
+    console.log (i+") " + array[i])}}
+    for(let i =0;i <array.length;i++){
+        if (array [i].includes("ll")){
+            console.log (i+") " + array[i])}}
+          console.log(array)  
